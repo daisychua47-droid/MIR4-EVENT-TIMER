503e32cb16efb9ffde44f7bb98d73d085a9446f4
daisychua47-droid <daisychua47@gmail.com>
2026-07-24 12:00:15 +0800

    Update api.js

--- api.js
+++ api.js
@@ -1,5 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbwxJB8AX32wXqwVZq7l6RZXfGSUKqiGR0XbCjWFI6WTKQb12xInk3Ruf_u15wsLb_fQsQ/exec
-";
+const API_URL = "https://script.google.com/macros/s/AKfycbwxJB8AX32wXqwVZq7l6RZXfGSUKqiGR0XbCjWFI6WTKQb12xInk3Ruf_u15wsLb_fQsQ/exec";
 
 async function getBosses() {
 
